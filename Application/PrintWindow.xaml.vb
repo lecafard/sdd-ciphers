ae15e96130215459c806fda0a9c21add999ddf00
Tom <lecafard@users.noreply.github.com>
2016-07-30 23:13:22 +1000

    lots of stuff today

--- Application/PrintWindow.xaml.vb
+++ Application/PrintWindow.xaml.vb
@@ -1,9 +1,68 @@
-﻿Public Class PrintWindow
+﻿Imports System
+Imports System.IO
+Imports System.Timers
+
+Public Class PrintWindow
     Public isClosed = False
+
+    ' crude rate limiting
+    Public LastPrintTime As DateTime = New DateTime(1970, 1, 1)
+    Dim warningTimer As System.Windows.Threading.DispatcherTimer
+    Dim oldBrush
+
+    ' wow, 10 seconds is long
+    Public PRINT_DELAY = 10000
 
     Private Sub btnBack_Click(sender As Object, e As RoutedEventArgs) Handles btnBack.Click
         isClosed = True
         Me.Close()
     End Sub
+
+    Private Sub btnCaesar_Click(sender As Object, e As RoutedEventArgs) Handles btnCaesar.Click
+        If (DateTime.UtcNow - LastPrintTime).TotalMilliseconds > PRINT_DELAY Then
+            SaveFile(My.Resources.caesar_print, Directory.GetCurrentDirectory() + "\caesar_print.pdf")
+            PrintFile("\caesar_print.pdf")
+            LastPrintTime = Date.UtcNow
+        Else
+            ' warn
+            oldBrush = btnCaesar.Background
+            btnCaesar.Background = (New BrushConverter().ConvertFrom("#FFFF0000"))
+            warningTimer = New System.Windows.Threading.DispatcherTimer()
+            warningTimer.Interval = New TimeSpan(0, 0, 0, 0, 250)
+            AddHandler warningTimer.Tick, AddressOf btnCaesar_Tick
+            warningTimer.Start()
+        End If
+
+    End Sub
+
+    Private Sub btnCaesar_Tick()
+        btnCaesar.Background = oldBrush
+        warningTimer.Stop()
+    End Sub
+
+    Private Sub btnVignere_Click(sender As Object, e As RoutedEventArgs) Handles btnVignere.Click
+        If (DateTime.UtcNow - LastPrintTime).TotalMilliseconds > PRINT_DELAY Then
+            SaveFile(My.Resources.vignere_print, Directory.GetCurrentDirectory() + "\vignere_print.pdf")
+            PrintFile("\vignere_print.pdf")
+            LastPrintTime = Date.UtcNow
+        Else
+            ' warn
+        End If
+    End Sub
+
+    Private Sub PrintFile(ByVal filename As String)
+        ' dang, this is nasty
+        Dim printProcess As New ProcessStartInfo
+        printProcess.UseShellExecute = True
+        printProcess.Verb = "print"
+        printProcess.FileName = Directory.GetCurrentDirectory() + filename
+        Process.Start(printProcess)
+    End Sub
+
+    Private Sub SaveFile(ByVal File As Object, ByVal FilePath As String)
+        Dim FByte() As Byte = File
+        My.Computer.FileSystem.WriteAllBytes(FilePath, FByte, True)
+    End Sub
+
 
 End Class
